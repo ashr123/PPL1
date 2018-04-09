fa800851b25ffce0e343239c1888f8a181aae422
Roy Ash <roykid123@gmail.com>
2018-04-09 18:09:55 +0300

    גדכדגכ

--- 1.3.js
+++ 1.3.js
@@ -2,4 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ramda_1 = require("ramda");
 let v1 = { name: "peter", age: 20 }, v2 = { children: [{ name: "john" }, { age: 12 }] }, v3 = (x) => x + 2, v4 = (f, l) => ramda_1.map((x) => f(f(x)), l);
+let check = [{ a: 5, b: {} }], check2 = [{ a: 5 }];
 //# sourceMappingURL=1.3.js.map
--- 1.3.ts
+++ 1.3.ts
@@ -4,3 +4,9 @@ let v1: { name: string, age: number }={name: "peter", age: 20},
 	v2: { children: ({ name: string } | { age: number })[] }={children: [{name: "john"}, {age: 12}]},
 	v3: (x: number) => number=(x) => x+2,
 	v4: <T, Y>(f: (x: T | Y) => T | Y, l: (T | Y)[]) => (T | Y)[]=(f, l) => map((x) => f(f(x)), l);
+
+type T1={ a: number, b: {} }[]
+type T2={ a: number }[]
+
+let check: T2=[{a: 5, b: {}}],
+	check2: T1=[{a: 5}];
