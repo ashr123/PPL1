7f13fedccf58a4a04b5086add826ea830a1a3862
Roy Ash <roykid123@gmail.com>
2018-04-16 16:57:51 +0300

    lkj

--- 1.3.js
+++ 1.3.js
@@ -2,14 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ramda_1 = require("ramda");
 const v1 = { name: "peter", age: 20 }, v2 = { children: [{ name: "john" }, { age: 12 }] }, v3 = (x) => x + 2, v4 = (f, l) => ramda_1.map((x) => f(f(x)), l);
-let a = [
-    [1, 2],
-    [3, 4],
-];
-let b = [
-    [9, 9],
-    [8, 7],
-];
-a.concat(b);
-console.log(b);
 //# sourceMappingURL=1.3.js.map
--- 1.3.ts
+++ 1.3.ts
@@ -4,13 +4,3 @@ const v1: { name: string, age: number }={name: "peter", age: 20},
 	v2: { children: ({ name: string } | { age: number })[] }={children: [{name: "john"}, {age: 12}]},
 	v3: (x: number) => number=(x) => x+2,
 	v4: <T, Y>(f: (x: T | Y) => T | Y, l: (T | Y)[]) => (T | Y)[]=(f, l) => map((x) => f(f(x)), l);
-let a=[
-	[1, 2],
-	[3, 4],
-];
-let b=[
-	[9, 9],
-	[8, 7],
-];
-a.concat(b);
-console.log(b);
